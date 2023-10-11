4c17ea769dd0bb2cf66265be81f9ff0d5b03328d
marinavasilcuk <marinabatenko@gmail.com>
2023-10-11 22:39:15 +0300

    Create add meal, delete meal, get meal by id, get page<MealDto> requests

---
+++ src/main/java/org/mvasylchuk/pfcc/domain/repository/MealJooqRepository.java
@@ -0,0 +1,59 @@
+package org.mvasylchuk.pfcc.domain.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.jooq.Condition;
+import org.jooq.DSLContext;
+import org.mvasylchuk.pfcc.common.dto.Page;
+import org.mvasylchuk.pfcc.common.dto.PfccDto;
+import org.mvasylchuk.pfcc.domain.dto.MealDto;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mvasylchuk.pfcc.jooq.tables.Meal.MEAL;
+
+@Component
+@RequiredArgsConstructor
+public class MealJooqRepository {
+    private final DSLContext ctx;
+
+    public Page<MealDto> getMealList(Integer page, Integer pageSize, LocalDateTime from, LocalDateTime to, Long userId) {
+        Page<MealDto> result = new Page<>();
+        result.setPage(page);
+        Integer totalElements = ctx.fetchCount(MEAL, MEAL.OWNER_ID.equal(userId)
+                .and(MEAL.EATEN_ON.between(from,to)));
+        result.setTotalElements(totalElements);
+        result.setPageSize(pageSize);
+        result.setTotalPages((totalElements / pageSize) + (totalElements % pageSize > 0 ? 1 : 0));
+
+        Condition condition = MEAL.OWNER_ID.equal(userId);
+        if (from != null) {
+            condition = condition.and(MEAL.EATEN_ON.greaterOrEqual(from));
+        }
+        if (to != null) {
+            condition = condition.and(MEAL.EATEN_ON.lessOrEqual(to));
+        }
+        List<MealDto> meals = ctx.selectFrom(MEAL)
+                .where(condition)
+                .limit(pageSize)
+                .offset(page * pageSize)
+
+                .fetch(dbMeal -> {
+                    MealDto meal = new MealDto();
+                    meal.setId(dbMeal.get(MEAL.ID));
+                    meal.setEatenOn(dbMeal.get(MEAL.EATEN_ON));
+                    meal.setWeight(dbMeal.get(MEAL.WEIGHT));
+                    meal.setPfcc(new PfccDto(dbMeal.get(MEAL.PROTEIN),
+                            dbMeal.get(MEAL.FAT),
+                            dbMeal.get(MEAL.CARBOHYDRATES),
+                            dbMeal.get(MEAL.CALORIES)));
+                    meal.setFoodId(dbMeal.get(MEAL.FOOD_ID));
+                    meal.setDishId(dbMeal.get(MEAL.DISH_ID));
+
+                    return meal;
+                });
+        result.setData(meals);
+        return result;
+    }
+}
